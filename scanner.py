e31c501deb4b4d4c4c3f49b5859eee1e945bd7b0
Matteo Alessio Carrara <sw.matteoac@gmail.com>
2016-12-12 00:15:35 +0100

    Aggiunta guida nello scanner

--- scanner.py
+++ scanner.py
@@ -4,11 +4,12 @@
 
 from os import system
 from time import sleep
-from sys import argv
+from sys import argv, stderr
 
 
 smpRate, resHoriz, resVert = [int(x) for x in argv[1:4]]
 
 for i in range(resHoriz * resVert):
+	stderr.write(str(i + 1) + "\n")
 	system("awk 'NR==3 {print $3}' /proc/net/wireless")
 	sleep(smpRate)
